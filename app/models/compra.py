f6363d4b3ce65e3243a176b6f66f4be331ebcad5
BlackNight414 <cristianariel375@gmail.com>
2024-10-28 20:57:49 -0300

    Se modifica el tipo de dato del campo fecha_compra de clase Compra a tipo datetime

--- app/models/compra.py
+++ app/models/compra.py
@@ -1,5 +1,5 @@
 from app import db
-from datetime import date, datetime
+from datetime import datetime
 
 class Compra(db.Model):
 
@@ -7,7 +7,7 @@ class Compra(db.Model):
 
     id: int = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     producto_id: int = db.Column('producto_id', db.Integer, nullable=False)
-    fecha_compra: date = db.Column('fecha_compra', db.Date, nullable=False, default=date.today())
+    fecha_compra: datetime = db.Column('fecha_compra', db.DateTime, nullable=False, default=datetime.today())
     direccion_envio: str = db.Column('direccion_envio', db.String(120), nullable=True) # puede no tener dirección
     fecha_eliminacion: datetime = db.Column('fecha_eliminacion', db.DateTime)
     observaciones: str = db.Column('observaciones', db.Text)
